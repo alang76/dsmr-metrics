c057b097c48c3ba1e81d0ad568478bf8dfee87f4
Arjen Langebaerd <alangebaerd@gmail.com>
2020-07-29 13:48:48 +0200

    Removed debug stuff

--- test/TestEffects/UpdatePrometheusMetric.hs
+++ test/TestEffects/UpdatePrometheusMetric.hs
@@ -7,20 +7,18 @@ import qualified Polysemy as P
 import qualified Polysemy.Output as P
 import Effects.UpdatePrometheusMetric(UpdatePrometheusMetric(..))
 
-import Debug.Trace(trace)
-
 runUpdatePrometheusMetricsFake :: P.Sem (UpdatePrometheusMetric:r) a -> P.Sem (P.Output String:r) a
 runUpdatePrometheusMetricsFake = 
         P.reinterpret $ \case
-            UpdateEnergyConsumedTariff2 _     -> trace ("updating the shizzle!") $ P.output "UpdateEnergyConsumedTariff2"
-            UpdateEnergyConsumedTariff1 _     -> trace ("updating the shizzle!") $ P.output "UpdateEnergyConsumedTariff1"
-            UpdateEnergyReturnedTariff1 _     -> trace ("updating the shizzle!") $ P.output "UpdateEnergyReturnedTariff1"
-            UpdateEnergyReturnedTariff2 _     -> trace ("updating the shizzle!") $ P.output "UpdateEnergyReturnedTariff2"
-            UpdateActualTariffIndicator _     -> trace ("updating the shizzle!") $ P.output "UpdateActualTariffIndicator"
-            UpdateActualPowerConsumption _    -> trace ("updating the shizzle!") $ P.output "UpdateActualPowerConsumption"
-            UpdateActualPowerReturned _       -> trace ("updating the shizzle!") $ P.output "UpdateActualPowerReturned"
-            UpdateNumberOfPowerFailures _     -> trace ("updating the shizzle!") $ P.output "UpdateNumberOfPowerFailures"
-            UpdateNumberOfPowerLongFailures _ -> trace ("updating the shizzle!") $ P.output "UpdateNumberOfPowerLongFailures"
-            UpdateActualCurrentConsumption _  -> trace ("updating the shizzle!") $ P.output "UpdateActualCurrentConsumption"
-            UpdateGasConsumption _ _          -> trace ("updating the shizzle!") $ P.output "UpdateGasConsumption"
-            UpdateNothing                     -> trace ("updating the shizzle!") $ P.output "UpdateNothing"
+            UpdateEnergyConsumedTariff2 _     -> P.output "UpdateEnergyConsumedTariff2"
+            UpdateEnergyConsumedTariff1 _     -> P.output "UpdateEnergyConsumedTariff1"
+            UpdateEnergyReturnedTariff1 _     -> P.output "UpdateEnergyReturnedTariff1"
+            UpdateEnergyReturnedTariff2 _     -> P.output "UpdateEnergyReturnedTariff2"
+            UpdateActualTariffIndicator _     -> P.output "UpdateActualTariffIndicator"
+            UpdateActualPowerConsumption _    -> P.output "UpdateActualPowerConsumption"
+            UpdateActualPowerReturned _       -> P.output "UpdateActualPowerReturned"
+            UpdateNumberOfPowerFailures _     -> P.output "UpdateNumberOfPowerFailures"
+            UpdateNumberOfPowerLongFailures _ -> P.output "UpdateNumberOfPowerLongFailures"
+            UpdateActualCurrentConsumption _  -> P.output "UpdateActualCurrentConsumption"
+            UpdateGasConsumption _ _          -> P.output "UpdateGasConsumption"
+            UpdateNothing                     -> P.output "UpdateNothing"
